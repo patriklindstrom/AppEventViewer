88792128794fa4d68a01817be2648fef4ec00eab
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-06-19 20:29:46 +0200

    Fixed concat of insertionstrings
    the String[] is in EventRecords is now concatenated.

--- AppEventViewer/Models/EventRecord.cs
+++ AppEventViewer/Models/EventRecord.cs
@@ -9,22 +9,36 @@ namespace AppEventViewer.Models
 {
     public class EventRecord
     {
-        
-       public  EventRecord(ManagementObject vmi) {
-           if (vmi == null) return; //vmi can be null eg for testing
-           Category = (vmi["Category"] != null) ? vmi["Category"].ToString() : String.Empty;
-           ComputerName = (vmi["ComputerName"] != null) ? vmi["ComputerName"].ToString() : String.Empty;
-           EventCode = (vmi["EventCode"] != null) ? vmi["EventCode"].ToString() : String.Empty;
-           EventType = (vmi["EventType"] != null) ? vmi["EventType"].ToString() : String.Empty;
-           InsertionStrings = (vmi["InsertionStrings"] != null) ? vmi["InsertionStrings"].ToString() : String.Empty;
-           Logfile = (vmi["Logfile"] != null) ? vmi["Logfile"].ToString() : String.Empty;
-           Message = (vmi["Message"] != null) ? vmi["Message"].ToString() : String.Empty;
-           RecordNumber = (vmi["RecordNumber"] != null) ? vmi["RecordNumber"].ToString() : String.Empty;
-           SourceName = (vmi["SourceName"] != null) ? vmi["SourceName"].ToString() : String.Empty;
-           TimeGenerated = (vmi["TimeGenerated"] != null) ? vmi["TimeGenerated"].ToString() : String.Empty;
-           TimeWritten = (vmi["TimeWritten"] != null) ? vmi["TimeWritten"].ToString() : String.Empty;
-           Type = (vmi["Type"] != null) ? vmi["Type"].ToString() : String.Empty;
-       }
+        public EventRecord(ManagementObject vmi)
+        {
+            if (vmi == null) return; //vmi can be null eg for testing
+            Category = (vmi["Category"] != null) ? vmi["Category"].ToString() : String.Empty;
+            ComputerName = (vmi["ComputerName"] != null) ? vmi["ComputerName"].ToString() : String.Empty;
+            EventCode = (vmi["EventCode"] != null) ? vmi["EventCode"].ToString() : String.Empty;
+            EventType = (vmi["EventType"] != null) ? vmi["EventType"].ToString() : String.Empty;
+
+
+            Logfile = (vmi["Logfile"] != null) ? vmi["Logfile"].ToString() : String.Empty;
+            Message = (vmi["Message"] != null) ? vmi["Message"].ToString() : String.Empty;
+            RecordNumber = (vmi["RecordNumber"] != null) ? vmi["RecordNumber"].ToString() : String.Empty;
+            SourceName = (vmi["SourceName"] != null) ? vmi["SourceName"].ToString() : String.Empty;
+            TimeGenerated = (vmi["TimeGenerated"] != null) ? vmi["TimeGenerated"].ToString() : String.Empty;
+            TimeWritten = (vmi["TimeWritten"] != null) ? vmi["TimeWritten"].ToString() : String.Empty;
+            Type = (vmi["Type"] != null) ? vmi["Type"].ToString() : String.Empty;
+            if (vmi["InsertionStrings"] != null )
+            {
+                var strList = (String[]) vmi["InsertionStrings"];
+                foreach (var insString in strList)
+                {
+                    this.InsertionStrings += insString;
+                }
+            }
+            else
+            {
+                this.InsertionStrings = String.Empty;
+            }
+        }
+
         public string Category { get; set; }
         public string ComputerName { get; set; }
         public string EventCode { get; set; }
