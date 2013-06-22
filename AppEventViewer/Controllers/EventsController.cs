eaaef6a29b7acbecb28429e49993be0ebebf317c
Patrik Lindstrom <patriklindstrom@hotmail.com>
2013-06-22 13:40:59 +0200

    EventsController and meta comments
    Added meta comments in code Started to add logic för the web gui
    EventsController

---
+++ AppEventViewer/Controllers/EventsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppEventViewer.ServiceInterface;
+
+namespace AppEventViewer.Controllers
+{
+    public class EventsController : Controller
+    {
+        //
+        // GET: /Events/
+        /// <summary>
+        /// The main get methods. Shows list of all events
+        /// </summary>
+        /// <param name="eventReq">A class for parameters they are strings but represent from to dates for the log events</param>
+        /// <seealso cref="EventReq"/>
+        public ActionResult Index(EventReq eventReq)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Events/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Events/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Events/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Events/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Events/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Events/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Events/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+    /// <summary>
+    /// A DTO class that stores the request parameters for the datetime from and to
+    /// similar to the Events class
+    /// </summary>
+    /// <seealso cref="AppEventViewer.ServiceInterface.Events"/>
+    public class EventReq
+    {
+        /// <summary>
+        /// Get the from date and time for log events 
+        /// </summary>
+        /// <value>The value should be able to parse as a date time</value>
+        public string   From { get; set; }
+        /// <summary>
+        /// Get the to in a from-to date and time for log events 
+        /// </summary>
+        /// <value>The value should be able to parse as a date time</value>
+        public string To { get; set; }
+    }
+}
--- AppEventViewer/ServiceInterface/EventService.cs
+++ AppEventViewer/ServiceInterface/EventService.cs
@@ -24,28 +24,30 @@ namespace AppEventViewer.ServiceInterface
     Notes = "What server nodes and the filter of events are hardcoded")]
     public class Events : IReturn<EventRecordListResponse>
     {
+        /// <summary>
+        /// Date and time from where the logs should be taken
+        /// </summary>
+        /// <value>The string must in yyyyMMddHHmmss eg 20130618102955 format to  be parsed as a DateTime. If null then defaults to - 1 hour</value>
         [ApiMember(Name = "from",
             Description =
                 "Date and time from where the logs should be taken. The string must in yyyyMMddHHmmss eg 20130618102955 format to  be parsed as a DateTime. If null then defaults to - 1 hour"
             ,
             ParameterType = "path", DataType = "string", IsRequired = false)]
         public string From { get; set; }
-
+        /// <summary>
+        /// Date and time To in a pair of from where the logs should be taken
+        /// </summary>
+        /// <value>Has to be bigger then the from value and The string must be The string must in yyyyMMddHHmmss eg 20130618102955 parsed as a DateTime. if null then defaults to now </value>
         [ApiMember(Name = "to",
             Description =
                 "Date and time to what where has to be bigger then the from value and The string must be The string must in yyyyMMddHHmmss eg 20130618102955 parsed as a DateTime. if null then defaults to now"
             ,
             ParameterType = "path", DataType = "string", IsRequired = false)]
         public string To { get; set; }
-
-        //public string SourceNameFilter { get; set; }
-        //public DateTime FromDate { get; set; }
-        //public DateTime ToDate { get; set; }
-        //public int MaxRows { get; set; }
     }
 
     /// <summary>
-    /// The Request DTO for the Get EventRecordList DTO
+    /// The Respons DTO for the Get EventRecordList DTO
     /// </summary>
     public class EventRecordListResponse
     {
@@ -54,11 +56,16 @@ namespace AppEventViewer.ServiceInterface
 
     //Implementation
     /// <summary>
-    /// 
+    /// This server get an optional pair of from and to datetimes strings. It then returns a List of filtered Events from logfile. It gets the data from a repository. This repository is injected 
+    /// outside of this class. It only implements the Get request. 
     /// </summary>
+    /// <seealso cref="Repository"/>
     public class EventService : Service
     {
-        public IEventRepository Repository { get; set; } // injected by Funq IOC in AppHost config                   
+        /// <summary>
+        /// It injected by Funq IOC in AppHost config or the Testprojects similar.
+        /// </summary>
+        public IEventRepository Repository { get; set; } //                   
         public object Get(Events request)
         {
             string fromTime = request.From ?? DateTime.Now.AddHours(-1).ToString(Global_Const.DATE_FORMAT);
